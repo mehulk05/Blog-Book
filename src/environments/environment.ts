bbab97881179d5cddbb9c75e61b5fcfc6e032d69
Mehul Kothari <34826782+mehulk05@users.noreply.github.com>
2020-07-18 20:00:40 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,17 @@
 
 export const environment = {
   production: false,
-     firebaseConfig :{
-    apiKey: "AIzaSyDwR0x3roioa6ZuEmOm8UwKTvmc08WDSfk",
-    authDomain: "write-your-heart-out-b338b.firebaseapp.com",
-    databaseURL: "https://write-your-heart-out-b338b.firebaseio.com",
-    projectId: "write-your-heart-out-b338b",
-    storageBucket: "write-your-heart-out-b338b.appspot.com",
-    messagingSenderId: "451879950201",
-    appId: "1:451879950201:web:b6e98720e6f8f2199201a8",
-    measurementId: "G-L1B51XC9TV"
+  
+  firebaseConfig :{
+ apiKey: "",
+ authDomain: "",
+ databaseURL: "",
+ projectId: "",
+ storageBucket: "",
+ messagingSenderId: "",
+ appId: "",
+ measurementId: ""
+
 }
 }
 /*
